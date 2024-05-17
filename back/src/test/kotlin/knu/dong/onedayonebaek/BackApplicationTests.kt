98e07e1a0a5ae9c5232ef87832ac36a316eefbbf
gidskql6671 <gidskql6671@gmail.com>
2024-05-17 14:58:26 +0900

    초기 세팅

---
+++ back/src/test/kotlin/knu/dong/onedayonebaek/BackApplicationTests.kt
@@ -0,0 +1,13 @@
+package knu.dong.onedayonebaek
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

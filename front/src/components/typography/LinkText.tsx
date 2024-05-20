b0aab269d36419b14c63c71d5b7749800a10325a
KimKyuHoi <k546kh@gmail.com>
2024-05-20 16:46:49 +0900

    feat: 메인 네비게이션 구현

--- front/src/components/typography/LinkText.tsx
+++ front/src/components/typography/LinkText.tsx
@@ -1,38 +1,35 @@
-import { MouseEventHandler } from 'react';
 import { ReactNode } from 'react';
+import { NavLink } from 'react-router-dom';
 
 import { ITextProps } from '@/interfaces/TextInterface';
 import styled from '@emotion/styled';
 
 interface ILinkTextContentProps extends ITextProps {
   children: ReactNode;
-  onClick: MouseEventHandler<HTMLSpanElement>;
   text?: string;
+  to: string;
 }
 
-const LinkTextContainer = styled.a<ILinkTextContentProps>`
+const LinkTextContainer = styled(NavLink)<ILinkTextContentProps>`
   font-size: ${({ size }) => size || '1rem'};
   font-weight: ${({ weight }) => weight || 'normal'};
   color: ${({ color }) => color || 'inherit'};
-  cursor: pointer;
   text-decoration: none;
-  &:hover {
-    text-decoration: none;
-  }
+  cursor: pointer;
 `;
 
 const LinkText = ({
   children,
   text,
-  onClick,
   size,
   weight,
+  to,
   color = 'var(--color-blue)',
   ...rest
 }: ILinkTextContentProps) => {
   return (
     <LinkTextContainer
-      onClick={onClick}
+      to={to}
       size={size}
       weight={weight}
       color={color}

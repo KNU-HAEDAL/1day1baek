301483298196aa7dbf7a861f8beb95f44807a2e6
KimKyuHoi <k546kh@gmail.com>
2024-05-23 19:15:34 +0900

    feat: 깃헙 로그인 구현

---
+++ front/src/pages/login/components/GithubButton.tsx
@@ -0,0 +1,42 @@
+import { BiLogoGithub } from 'react-icons/bi';
+
+import { DefaultButton } from '@components/button/DefaultButton';
+
+import styled from '@emotion/styled';
+
+const GithubStyledButton = styled(DefaultButton)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 420px;
+  height: 40px;
+  border-radius: 12px;
+  background-color: var(--color-black);
+  color: var(--color-white);
+  gap: 8px;
+
+  &:hover {
+    background-color: var(--color-black);
+    color: var(--color-white);
+  }
+
+  &:focus {
+    outline: none;
+    background-color: var(--color-black);
+  }
+`;
+
+const GithubButton = () => {
+  const onClickLogin = () => {
+    window.location.href = 'http://localhost:8080/oauth2/authorization/github';
+  };
+
+  return (
+    <GithubStyledButton onClick={onClickLogin}>
+      <BiLogoGithub size={24} />
+      github로 로그인하기
+    </GithubStyledButton>
+  );
+};
+
+export default GithubButton;

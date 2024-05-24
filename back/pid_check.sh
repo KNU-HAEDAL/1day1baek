390f7499f14fe8f232a79cb7d25d4b51ff2b5dac
gidskql6671 <gidskql6671@gmail.com>
2024-05-24 10:57:56 +0900

    production 서버용 스크립트 추가

---
+++ back/pid_check.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ps -ef | grep OneDayOneBaek-0.0.1-SNAPSHOT.jar
--- back/run.sh
+++ back/run.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 ./gradlew build
-java -Dspring.profiles.active=prod -jar build/libs/OneDayOneBaek-0.0.1-SNAPSHOT.jar
+nohup java -Dspring.profiles.active=prod -jar build/libs/OneDayOneBaek-0.0.1-SNAPSHOT.jar > log.txt 2>&1 &

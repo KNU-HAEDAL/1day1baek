fcb27861f7bcb77f22d5f8b45056716e948e3f56
gidskql6671 <gidskql6671@gmail.com>
2024-05-24 23:51:44 +0900

    server timezone 설정

--- back/src/main/kotlin/knu/dong/onedayonebaek/BackApplication.kt
+++ back/src/main/kotlin/knu/dong/onedayonebaek/BackApplication.kt
@@ -1,10 +1,17 @@
 package knu.dong.onedayonebaek
 
+import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.*
 
 @SpringBootApplication
 class BackApplication
+
+@PostConstruct
+fun started() {
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
+}
 
 fun main(args: Array<String>) {
     runApplication<BackApplication>(*args)

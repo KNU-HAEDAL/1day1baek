d2775a8470de0fff6ac11c8eb57fe491424432ab
gidskql6671 <gidskql6671@gmail.com>
2024-05-28 00:12:30 +0900

    commits -> problems 도메인 이름 변경

--- back/src/main/kotlin/knu/dong/onedayonebaek/service/ProblemService.kt
+++ back/src/main/kotlin/knu/dong/onedayonebaek/service/ProblemService.kt
@@ -2,9 +2,9 @@ package knu.dong.onedayonebaek.service
 
 import io.github.oshai.kotlinlogging.KotlinLogging
 import knu.dong.onedayonebaek.domain.User
-import knu.dong.onedayonebaek.dto.CommitInfo
-import knu.dong.onedayonebaek.dto.toCommitInfo
-import knu.dong.onedayonebaek.repository.CommitRepository
+import knu.dong.onedayonebaek.dto.ProblemDto
+import knu.dong.onedayonebaek.dto.toProblemDto
+import knu.dong.onedayonebaek.repository.ProblemRepository
 import knu.dong.onedayonebaek.repository.UserRepository
 import org.springframework.stereotype.Service
 import java.time.LocalDate
@@ -14,27 +14,27 @@ import java.util.stream.Collectors
 private val myLogger = KotlinLogging.logger {}
 
 @Service
-class CommitService(
-    private val commitRepository: CommitRepository,
+class ProblemService(
+    private val problemRepository: ProblemRepository,
     private val userRepository: UserRepository
 ) {
 
-    fun getCommits(user: User, yearMonth: YearMonth): List<CommitInfo> {
+    fun getProblems(user: User, yearMonth: YearMonth): List<ProblemDto> {
         val start = yearMonth.atDay(1)
         val end = yearMonth.atEndOfMonth()
 
-        return commitRepository
-            .findAllByCommitDateBetweenAndUser(start, end, user)
+        return problemRepository
+            .findAllBySolvedDateBetweenAndUser(start, end, user)
             .stream()
-            .map { it.toCommitInfo() }
+            .map { it.toProblemDto() }
             .collect(Collectors.toList())
     }
 
-    fun getCommits(user: User, date: LocalDate): List<CommitInfo> {
-        return commitRepository
-            .findAllByCommitDateAndUser(date, user)
+    fun getProblems(user: User, date: LocalDate): List<ProblemDto> {
+        return problemRepository
+            .findAllBySolvedDateAndUser(date, user)
             .stream()
-            .map { it.toCommitInfo() }
+            .map { it.toProblemDto() }
             .collect(Collectors.toList())
     }
 }

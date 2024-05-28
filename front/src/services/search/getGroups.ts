c16c8813c73d5c1da0c40fbba7d620551557b748
KimKyuHoi <k546kh@gmail.com>
2024-05-29 07:24:39 +0900

    rename: 파일 확장자 변경

---
+++ front/src/services/search/getGroups.ts
@@ -0,0 +1,13 @@
+import { BASE_URI } from '@constants/URI';
+
+import { instance } from '@services/API_JWT';
+
+export const getGroups = async () => {
+  try {
+    const response = await instance.get(`${BASE_URI}/groups`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching groups Try Again:', error);
+    throw error;
+  }
+};

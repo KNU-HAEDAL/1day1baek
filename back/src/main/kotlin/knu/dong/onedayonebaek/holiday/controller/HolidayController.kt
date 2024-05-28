21c7479359a386823dcd32ef5c8257432f78a613
gidskql6671 <gidskql6671@gmail.com>
2024-05-29 00:47:21 +0900

    특정 범위의 일자들을 모두 휴일로 만드는 API 구현

--- back/src/main/kotlin/knu/dong/onedayonebaek/holiday/controller/HolidayController.kt
+++ back/src/main/kotlin/knu/dong/onedayonebaek/holiday/controller/HolidayController.kt
@@ -2,9 +2,11 @@ package knu.dong.onedayonebaek.holiday.controller
 
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
+import knu.dong.onedayonebaek.holiday.dto.AddHolidaysBetweenRequest
 import knu.dong.onedayonebaek.holiday.dto.AddHolidaysRequest
 import knu.dong.onedayonebaek.holiday.service.HolidayService
 import org.springframework.web.bind.annotation.*
+import java.time.LocalDate
 
 @RestController
 @RequestMapping("/holidays")
@@ -14,10 +16,31 @@ class HolidayController(private val holidayService: HolidayService) {
     @Operation(
         hidden = true,
         summary = "휴일 추가 API",
-        description = "휴일을 추가한다. (권한 추가를 안해서 사용하지 마십숑)"
+        description = "휴일들을 추가한다. (권한 추가를 안해서 사용하지 마십숑)"
     )
     @PostMapping
     fun addHolidays(@RequestBody request: AddHolidaysRequest) {
         holidayService.addHolidays(request.holidays)
+    }
+
+    @Operation(
+        hidden = true,
+        summary = "휴일 범위로 추가 API",
+        description = "특정 범위를 모두 휴일로 추가한다. (권한 추가를 안해서 사용하지 마십숑)"
+    )
+    @PostMapping("/range")
+    fun addHolidaysBetween(@RequestBody request: AddHolidaysBetweenRequest) {
+        val start: LocalDate = request.start
+        val end: LocalDate = request.end
+
+        val dates: MutableList<LocalDate> = ArrayList()
+
+        var date = start
+        while (!date.isAfter(end)) {
+            dates.add(date)
+            date = date.plusDays(1)
+        }
+
+        holidayService.addHolidays(dates)
     }
 }

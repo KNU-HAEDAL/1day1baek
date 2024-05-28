98ec4b0e72f6df686bf5835ca3dcc1cd72db0c17
KimKyuHoi <k546kh@gmail.com>
2024-05-29 03:46:14 +0900

    env: ToastProvider 환경 구축

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,5 +1,7 @@
 import ReactDOM from 'react-dom/client';
 
+import ToastProvider from '@components/alarm/ToastProvider';
+
 import App from '@/App';
 import { worker } from '@mocks/Browser';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -19,7 +21,9 @@ const enableMocking = async () => {
 enableMocking().then(() => {
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ToastProvider>
+        <App />
+      </ToastProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );

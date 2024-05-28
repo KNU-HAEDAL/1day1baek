07b36ee35b06fe98a76c659710e195b9b04bdd85
gidskql6671 <gidskql6671@gmail.com>
2024-05-29 00:31:04 +0900

    리퀘스트 타입 버그 수정

--- back/src/main/kotlin/knu/dong/onedayonebaek/github/dto/CommitWebhookRequest.kt
+++ back/src/main/kotlin/knu/dong/onedayonebaek/github/dto/CommitWebhookRequest.kt
@@ -1,11 +1,11 @@
 package knu.dong.onedayonebaek.github.dto
 
-import java.time.LocalDateTime
+import java.time.ZonedDateTime
 
 data class CommitWebhookRequest(
     val pusher: Pusher,
     val commits: List<Commit> = ArrayList()
 ) {
-    data class Commit (val url: String, val message: String, val timestamp: LocalDateTime)
+    data class Commit (val url: String, val message: String, val timestamp: ZonedDateTime)
     data class Pusher (val name: String, val email: String)
 }

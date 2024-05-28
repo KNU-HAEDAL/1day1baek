a853dc0053b944dd4e6f80acbd2ed4a01d2f5924
KimKyuHoi <k546kh@gmail.com>
2024-05-29 06:09:13 +0900

    test: msw 코드 작성

--- front/src/mocks/handler/Handler.ts
+++ front/src/mocks/handler/Handler.ts
@@ -1,3 +1,4 @@
+import { SearchHandler } from '@mocks/handler/search/SearchHandler';
 import { UserHandler } from '@mocks/handler/user/UserHandler';
 
-export const Handlers = [...UserHandler];
+export const Handlers = [...UserHandler, ...SearchHandler];

ddb27b86bad08d7a3b75fa0063e486672337a517
KimKyuHoi <k546kh@gmail.com>
2024-05-29 19:18:46 +0900

    fix: queryKey 수정

--- front/src/hooks/queries/search/getGroupsQuery.ts
+++ front/src/hooks/queries/search/getGroupsQuery.ts
@@ -3,11 +3,11 @@ import { getGroups } from '@services/search/getGroups';
 import { useQuery } from '@tanstack/react-query';
 
 export const useGroups = () => {
-  const { data, isPending, isError, error } = useQuery({
-    queryKey: ['groups'],
+  const { data, isPending, isError, error, refetch } = useQuery({
+    queryKey: ['users-groups'],
     queryFn: getGroups,
     refetchOnMount: false,
   });
 
-  return { data, isPending, isError, error };
+  return { data, isPending, isError, error, refetch };
 };

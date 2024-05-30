222df81bf7a8ea3cf0eb0e26fa2c46cf257ed058
KimKyuHoi <k546kh@gmail.com>
2024-05-30 13:22:45 +0900

    style: 공통 css 분리

--- front/src/pages/group/components/CreateModal.tsx
+++ front/src/pages/group/components/CreateModal.tsx
@@ -11,51 +11,20 @@ import {
 
 import { ICreateGroupModalProps } from '@interfaces/GroupInterface';
 
+import {
+  ModalContainer,
+  ModalTitle,
+  FormGroup,
+  InputLabel,
+  InputField,
+  Error,
+  Button,
+  CloseButton,
+} from '@styles/ModalLayout';
+
 import styled from '@emotion/styled';
 import { ErrorMessage } from '@hookform/error-message';
 import { zodResolver } from '@hookform/resolvers/zod';
-
-const ModalContainer = styled.div`
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background-color: #fff;
-  border-radius: 8px;
-  padding: 20px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-  width: 400px;
-`;
-
-const ModalTitle = styled.h2`
-  font-size: var(--size-md);
-  margin-bottom: 20px;
-`;
-
-const Error = styled.small`
-  color: var(--color-red);
-  font-size: var(--size-xs);
-  margin: 0px 0px 12px 6px;
-`;
-
-const FormGroup = styled.div`
-  margin-bottom: 20px;
-`;
-
-const InputLabel = styled.label`
-  display: block;
-  margin-bottom: 5px;
-  font-weight: bold;
-  font-size: var(--size-xs);
-`;
-
-const InputField = styled.input`
-  width: calc(100% - 16px);
-  padding: 8px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  margin-top: 5px;
-`;
 
 const CheckboxLabel = styled.label`
   display: flex;
@@ -66,26 +35,6 @@ const CheckboxLabel = styled.label`
 
 const CheckboxInput = styled.input`
   margin-right: 5px;
-`;
-
-const Button = styled.button`
-  padding: 8px 16px;
-  border: none;
-  border-radius: 4px;
-  background-color: var(--color-blue);
-  color: white;
-  cursor: pointer;
-`;
-
-const CloseButton = styled.button`
-  position: absolute;
-  top: 10px;
-  right: 10px;
-  padding: 5px;
-  border: none;
-  border-radius: 50%;
-  background-color: transparent;
-  cursor: pointer;
 `;
 
 const CreateModal = ({ closeModal }: { closeModal: () => void }) => {
